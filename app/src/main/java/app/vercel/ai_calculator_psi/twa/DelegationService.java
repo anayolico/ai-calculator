8461752fc1a4b72f712af6b3dac90602c8300bad
anayolico <acnwa1234@gmail.com>
2025-12-04 19:35:22 +0100

    Add updated manifest.json, maskable icon, and Android Bubblewrap project files

---
+++ app/src/main/java/app/vercel/ai_calculator_psi/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.ai_calculator_psi.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
